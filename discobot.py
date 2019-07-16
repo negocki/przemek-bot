c26c0f713d7a150e85d65e7f41d485718bae348d
Wojciech K <shcwojti@gmail.com>
2019-07-16 23:23:24 +0200

    Updated to newest discord.py, works except minus5

--- discobot.py
+++ discobot.py
@@ -21,43 +21,44 @@ startup_time = datetime.now()
 
 @client.event
 async def on_message(message):
-    # we do not want the bot to reply to itself
+
     if message.author == client.user:
         return
 
     if message.content.startswith('!hello'):
         msg = 'Spierdalaj {0.author.mention}'.format(message)
-        await client.send_message(message.channel, msg)
+        await message.channel.send(msg)
 
     if message.content.startswith('!help'):
-        msg = 'Lista komend:\n!wykop - losowy wpis z gorących mirko \n !cytat - losowy cytat \n !cytat [numer] - konkretny cytat \n !addcytat - dodawanie cytatu'
-        await client.send_message(message.channel, msg)
+        msg = '```Lista komend:\n!wykop - losowy wpis z gorących mirko \n!cytat - losowy cytat \n!cytat [numer] - konkretny cytat \n!addcytat - dodawanie cytatu```'
+        await message.channel.send(msg)
 
     if message.content.startswith('!wykop'):  # mirko
-        entry = wykop.get_random_hot()
-        msg = '```@{} - {} - {} głosów```\n{}'.format(entry[1],entry[4],entry[3],entry[5])
-        await client.send_message(message.channel, msg)
+       entry = wykop.get_random_hot()
+       msg = '```@{} - {} - {} głosów```\n{}'.format(entry[1],entry[4],entry[3],entry[5])
+       await message.channel.send(msg)
 
     if message.content.startswith('!bachu'):
         msg = 'http://i.imgur.com/lOx73f1.jpg'.format(message)
-        await client.send_message(message.channel, msg)
+        await message.channel.send(msg)
 
     if message.content.startswith('!goha'):
         msg = 'go ha go ha go ha go ha 3 zł'.format(message)
-        await client.send_message(message.channel, msg, tts=True)
+        await message.channel.send(msg)
+        #await client.send_message(message.channel, msg, tts=True)
 
     if message.content.startswith('!pszemek'):
         msg = 'https://cdn.discordapp.com/attachments/338355995124301824/353985149890854912/DSC_0160.JPG'.format(message)
-        await client.send_message(message.channel, msg)
+        await message.channel.send(msg)
 
     if message.content.startswith('!makaron'):
         msg = 'https://cdn.discordapp.com/attachments/355434446398160897/355435945824092161/ZVKjmto.jpg'.format(message)
-        await client.send_message(message.channel, msg)
+        await message.channel.send(msg)
 
     if message.content.startswith('!addcytat'):  # dodawanie cytatu
 
         cytat_n = ("","")
-        async for message_log in client.logs_from(message.channel, limit=100): # odczytujemy poprzednia wiadomosc
+        async for message_log in message.channel.history(limit=100): # odczytujemy poprzednia wiadomosc
             if message_log.author != client.user and message_log.author != message.author:
                 cytat_n = (message_log.author, message_log.content)
                 break
@@ -76,24 +77,24 @@ async def on_message(message):
         else:
             msg = 'Nie znaleziono cytatu'
 
-        await client.send_message(message.channel, msg)
-        
+        await message.channel.send(msg)
+
     if message.content.startswith('!topminus5'):
         stats = sorted(minus5.get_stats(), key=lambda x: x[1], reverse=True)
         msg = 'Statystyki obrażania matek:\n'
         for user_id, count in stats:
-            user_data = await client.get_user_info(user_id)
+            user_data = await client.get_user_info(user_id)  #tu error
             msg += user_data.display_name + ': ' + str(count * -5) + ' punktów\n'
-            
-        await client.send_message(message.channel, msg)
-        
-    if message.content.startswith('!recalcminus5') and (message.author.name == "negocki" or message.author.name == "Kristopher38"):
+
+        await message.channel.send(msg)
+
+    if message.content.startswith('!recalcminus5') and (message.author.name == "negocki" or message.author.name == "Kristopher38" or message.author.name == "w0jt1"):
         print('Recalculating minus5 stats, this might take a while')
         server_logs = []
         recalc_stats = {}
-        
+
         for channel in client.get_all_channels():
-            channel_logs = client.logs_from(channel, limit=10000, before=startup_time) # hardcoded 10k messages limit
+            channel_logs = client.logs_from(channel, limit=10000, before=startup_time) # hardcoded 10k messages limit #drugi error
             async for msg in channel_logs:
                 for reaction in msg.reactions:
                     if reaction.custom_emoji:
@@ -102,30 +103,36 @@ async def on_message(message):
                                 recalc_stats[msg.author.id] += reaction.count
                             else:
                                 recalc_stats[msg.author.id] = reaction.count
-                                
+
         minus5.recalculate_stats(recalc_stats)
         print('Recalculating finished')
-        await client.send_message(message.channel, 'Recalculating finished')
-            
+        await message.channel.send('Recalculating finished')
+
 @client.event
 async def on_ready():
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
     print('------')
-    playing = discord.Game(name="!help")
-    await client.change_presence(game=playing)
+    await client.change_presence(activity=discord.Game(name="!help"))
+
+#tez nie dziala xD
 
 @client.event
 async def on_reaction_add(reaction, user):
     if reaction.custom_emoji:
         if reaction.emoji.name == "minus5":
             minus5.increment_user(reaction.message.author)
-        
+            print('Dodano emotke')
+
 @client.event
 async def on_reaction_remove(reaction, user):
     if reaction.custom_emoji:
         if reaction.emoji.name == "minus5":
             minus5.decrement_user(reaction.message.author)
+            print('usunieto emotke')
 
-client.run('MzU1NDMyNTY2ODIxNjE3Njc1.DJMt-Q.FKP0s7QFgcHnCKd8kHODwMqe3hk')
+client.run('')
+
+
+
--- minus5.py
+++ minus5.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 class Minus5:
     def __init__(self, conn):
         self.conn = conn
@@ -10,43 +11,43 @@ class Minus5:
         if len(self.curs.fetchall()) == 0:
             self.curs.execute("CREATE TABLE minus5 (user_id text, count int)")
             self.conn.commit()
-            
+
     def _add_user(self, user_id):
         self.curs.execute("INSERT INTO minus5 (user_id, count) VALUES (?, 0)", (user_id,))
         self.conn.commit()
-        
+
     def _remove_user(self, user_id):
         self.curs.execute("DELETE FROM minus5 WHERE user_id=?", (user_id,))
         self.conn.commit()
-    
+
     def _is_user_in_database(self, user_id):
         self.curs.execute("SELECT * FROM minus5 WHERE user_id=?", (user_id,))
         if len(self.curs.fetchall()) < 1:
             return False
         else:
             return True
-        
+
     def recalculate_stats(self, stats_dict):
-        self.curs.execute("DELETE FROM minus5") # delete all records
+        self.curs.execute("DELETE FROM minus5")  # delete all records
         for user_id, count in stats_dict.items():
             self._add_user(user_id)
             self.curs.execute("UPDATE minus5 SET count=? WHERE user_id=?", (count, user_id))
         self.conn.commit()
-        
+
     def get_stats(self, user=None):
         if user == None:
             self.curs.execute("SELECT user_id, count FROM minus5")
             return self.curs.fetchall()
         else:
             self.curs.execute("SELECT user_id, count FROM minus5 WHERE user_id=?", (user.id,))
-            return self.curs.fetchone() 
-        
+            return self.curs.fetchone()
+
     def increment_user(self, user):
         if not self._is_user_in_database(user.id):
             self._add_user(user.id)
         self.curs.execute("UPDATE minus5 SET count=count+1 WHERE user_id=?", (user.id,))
         self.conn.commit()
-        
+
     def decrement_user(self, user):
         self.curs.execute("UPDATE minus5 SET count=count-1 WHERE user_id=?", (user.id,))
         self.conn.commit()
